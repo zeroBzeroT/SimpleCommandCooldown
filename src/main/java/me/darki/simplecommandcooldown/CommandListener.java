f708b01a4d62777c6d80644af4aca47d12546fa0
blockparole <48992448+blockparole@users.noreply.github.com>
2020-07-13 16:09:39 +0200

    use uuid instead of full player object as pair key

--- src/main/java/me/darki/simplecommandcooldown/CommandListener.java
+++ src/main/java/me/darki/simplecommandcooldown/CommandListener.java
@@ -9,13 +9,13 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.AbstractMap;
-import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CommandListener implements Listener {
 
-    final private ConcurrentHashMap<AbstractMap.SimpleEntry<Player, String>, Long> cooldownMap;
+    final private ConcurrentHashMap<AbstractMap.SimpleEntry<UUID, String>, Long> cooldownMap;
     final private ReentrantLock mutex;
 
     public CommandListener() {
@@ -29,32 +29,35 @@ public class CommandListener implements Listener {
         Player player = event.getPlayer();
         String commandName = event.getMessage().split(" ")[0];
 
-        AbstractMap.SimpleEntry<Player, String> pair = new AbstractMap.SimpleEntry<>(player, commandName);
+        AbstractMap.SimpleEntry<UUID, String> pair = new AbstractMap.SimpleEntry<>(player.getUniqueId(), commandName);
 
         try {
-            this.mutex.lock();
 
-            if (this.cooldownMap.containsKey(pair)) {
-                if (System.currentTimeMillis() - this.cooldownMap.get(pair) > SimpleCommandCooldown.cooldown) {
-                    this.cooldownMap.remove(pair);
+            mutex.lock();
+
+            if (cooldownMap.containsKey(pair)) {
+                if (System.currentTimeMillis() - cooldownMap.get(pair) > SimpleCommandCooldown.cooldown) {
+                    cooldownMap.remove(pair);
                 } else {
                     player.sendMessage(ChatColor.RED + "Please wait a bit before using this command again!");
                     event.setCancelled(true);
                 }
             } else {
-                this.cooldownMap.put(pair, System.currentTimeMillis());
+                cooldownMap.put(pair, System.currentTimeMillis());
             }
+
         } finally {
-            this.mutex.unlock();
+            mutex.unlock();
         }
+
     }
 
     @EventHandler(ignoreCancelled = true)
     public void onPlayerQuit(PlayerQuitEvent event) {
-        for (Map.Entry<AbstractMap.SimpleEntry<Player, String>, Long> entry : this.cooldownMap.entrySet()) {
-            if (entry.getKey().getKey() == event.getPlayer())
-                this.cooldownMap.remove(entry.getKey());
-        }
+        cooldownMap.forEach((data, timeout) -> {
+            if (data.getKey().equals(event.getPlayer().getUniqueId()))
+                cooldownMap.remove(data);
+        });
     }
 
 }
