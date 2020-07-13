957af6dcbcd2446944848ae1b3c7a67db65893bf
blockparole <48992448+blockparole@users.noreply.github.com>
2020-07-13 16:26:36 +0200

    use class instead of AbstractMap.SimpleEntry

--- src/main/java/me/darki/simplecommandcooldown/CommandListener.java
+++ src/main/java/me/darki/simplecommandcooldown/CommandListener.java
@@ -8,14 +8,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.util.AbstractMap;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CommandListener implements Listener {
 
-    final private ConcurrentHashMap<AbstractMap.SimpleEntry<UUID, String>, Long> cooldownMap;
+    final private ConcurrentHashMap<Data, Long> cooldownMap;
     final private ReentrantLock mutex;
 
     public CommandListener() {
@@ -28,20 +27,19 @@ public class CommandListener implements Listener {
 
         Player player = event.getPlayer();
         String commandName = event.getMessage().split(" ")[0];
-
-        AbstractMap.SimpleEntry<UUID, String> pair = new AbstractMap.SimpleEntry<>(player.getUniqueId(), commandName);
+        Data data = new Data(player.getUniqueId(), commandName);
 
         try {
 
             mutex.lock();
 
-            if (!cooldownMap.containsKey(pair)) {
-                cooldownMap.put(pair, System.currentTimeMillis());
+            if (!cooldownMap.containsKey(data)) {
+                cooldownMap.put(data, System.currentTimeMillis());
                 return;
             }
 
-            if (System.currentTimeMillis() - cooldownMap.get(pair) > SimpleCommandCooldown.cooldown) {
-                cooldownMap.remove(pair);
+            if (System.currentTimeMillis() - cooldownMap.get(data) > SimpleCommandCooldown.cooldown) {
+                cooldownMap.remove(data);
             } else {
                 player.sendMessage(ChatColor.RED + "Please wait a bit before using this command again!");
                 event.setCancelled(true);
@@ -56,9 +54,29 @@ public class CommandListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerQuit(PlayerQuitEvent event) {
         cooldownMap.forEach((data, timeout) -> {
-            if (data.getKey().equals(event.getPlayer().getUniqueId()))
+            if (data.getUuid().equals(event.getPlayer().getUniqueId()))
                 cooldownMap.remove(data);
         });
+    }
+
+    public static class Data {
+
+        private final UUID uuid;
+        private final String command;
+
+        public Data(UUID uuid, String command) {
+            this.uuid = uuid;
+            this.command = command;
+        }
+
+        public UUID getUuid() {
+            return uuid;
+        }
+
+        public String getCommand() {
+            return command;
+        }
+
     }
 
 }
