9099704244c0e4e740e19461769ea5892c9fd440
blockparole <48992448+blockparole@users.noreply.github.com>
2020-07-13 15:58:06 +0200

    replace javafx pair with java.util.AbstractMap.SimpleEntry

--- src/main/java/me/darki/simplecommandcooldown/CommandListener.java
+++ src/main/java/me/darki/simplecommandcooldown/CommandListener.java
@@ -1,6 +1,5 @@
 package me.darki.simplecommandcooldown;
 
-import javafx.util.Pair;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,17 +8,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.AbstractMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CommandListener implements Listener {
 
-    final private ConcurrentHashMap<Pair<Player, String>, Long> cooldownMap;
-    final private ReentrantLock mutex = new ReentrantLock();
+    final private ConcurrentHashMap<AbstractMap.SimpleEntry<Player, String>, Long> cooldownMap;
+    final private ReentrantLock mutex;
 
     public CommandListener() {
         cooldownMap = new ConcurrentHashMap<>();
+        mutex = new ReentrantLock();
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -28,7 +29,7 @@ public class CommandListener implements Listener {
         Player player = event.getPlayer();
         String commandName = event.getMessage().split(" ")[0];
 
-        Pair<Player, String> pair = new Pair<>(player, commandName);
+        AbstractMap.SimpleEntry<Player, String> pair = new AbstractMap.SimpleEntry<>(player, commandName);
 
         try {
             this.mutex.lock();
@@ -53,7 +54,7 @@ public class CommandListener implements Listener {
 
         Player player = event.getPlayer();
 
-        for (Map.Entry<Pair<Player, String>, Long> entry : this.cooldownMap.entrySet()) {
+        for (Map.Entry<AbstractMap.SimpleEntry<Player, String>, Long> entry : this.cooldownMap.entrySet()) {
             if (entry.getKey().getKey() == player)
                 this.cooldownMap.remove(entry.getKey());
         }
